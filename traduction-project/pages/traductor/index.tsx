7799c9a143f64e0090e6bf494eb27b357aa90f85
Josue Hernández <josue.hernandez@geekinc.mx>
2023-08-21 00:28:05 -0600

    Traduccion page terminada

--- traduction-project/pages/traductor/index.tsx
+++ traduction-project/pages/traductor/index.tsx
@@ -7,7 +7,24 @@ import services1 from "@/assets/path8014.webp";
 import services2 from "@/assets/layer1-9bD.webp";
 import services3 from "@/assets/layer1.webp";
 
+import traduction1 from "@/assets/image8243.png";
+import traduction2 from "@/assets/image8231.png";
+import traduction3 from "@/assets/image8279.png";
+import traduction4 from "@/assets/image8267.png";
+import traduction5 from "@/assets/image8255.png";
+
+import arrow from "@/assets/vector-5.webp";
+import { useState } from "react";
+import { Switch } from "@headlessui/react";
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
+
 export default function Traduction() {
+  const [enabledEspToIng, setEnabledEspToIng] = useState(false);
+  const [enabledIngToEsp, setEnabledIngToEsp] = useState(false);
+
   return (
     <Layout>
       {
@@ -34,7 +51,7 @@ export default function Traduction() {
           <div className="relative isolate pt-14">
             <div className="mx-auto max-w-7xl px-8 lg:px-16">
               <div className="mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-                <div className="flex flex-col items-start bg-white shadow-md border rounded-xl px-4 py-8 text-center">
+                <div className="flex flex-col items-center bg-white shadow-md border rounded-xl px-4 py-8 text-center">
                   <h3 className="text-2xl font-bold tracking-tight text-gray-900 text-center">
                     ¡Solicita una cotización hoy mismo y traduce tu documento
                     con la mejor calidad y precio!
@@ -42,6 +59,68 @@ export default function Traduction() {
                   <h5 className="text-xl font-bold tracking-tight text-gray-600 text-center mt-8 mx-auto">
                     ¿Qué tipo de traducción necesitas?
                   </h5>
+                  <div className="py-5 w-full">
+                    <div className="flex my-5">
+                      <div className="flex-auto w-full">
+                        <p className="text-md text-left font-semibold tracking-tight text-gray-700">
+                          Español-Ingles
+                        </p>
+                      </div>
+                      <div className="flex-none">
+                        <Switch
+                          checked={enabledEspToIng}
+                          onChange={setEnabledEspToIng}
+                          className={classNames(
+                            enabledEspToIng ? "bg-sky-600" : "bg-gray-200",
+                            "relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2"
+                          )}
+                        >
+                          <span
+                            aria-hidden="true"
+                            className={classNames(
+                              enabledEspToIng
+                                ? "translate-x-5"
+                                : "translate-x-0",
+                              "pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out"
+                            )}
+                          />
+                        </Switch>
+                      </div>
+                    </div>
+                    <div className="flex my-5">
+                      <div className="flex-auto">
+                        <p className="text-md text-left font-semibold tracking-tight text-gray-700">
+                          Ingles-Español
+                        </p>
+                      </div>
+                      <div className="flex-none">
+                        <Switch
+                          checked={enabledIngToEsp}
+                          onChange={setEnabledIngToEsp}
+                          className={classNames(
+                            enabledIngToEsp ? "bg-sky-600" : "bg-gray-200",
+                            "relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2"
+                          )}
+                        >
+                          <span
+                            aria-hidden="true"
+                            className={classNames(
+                              enabledIngToEsp
+                                ? "translate-x-5"
+                                : "translate-x-0",
+                              "pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out"
+                            )}
+                          />
+                        </Switch>
+                      </div>
+                    </div>
+                    <button
+                      type="submit"
+                      className="mt-6 block w-full rounded-lg footer-bg px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm"
+                    >
+                      Solicita tu cotización
+                    </button>
+                  </div>
                   {/* <div className="grid grid-cols-1 md:grid-cols-2"></div> */}
                 </div>
                 <div className="flex flex-col items-start justify-between bg-white shadow-md border rounded-xl px-4 py-8">
@@ -96,7 +175,7 @@ export default function Traduction() {
             </div>
           </div>
           <div className="bg-white">
-            <div className="mx-auto max-w-7xl px-4 py-24 sm:px-6 lg:px-8">
+            <div className="mx-auto max-w-7xl px-4 py-12 sm:px-6 lg:px-8">
               <div className="rounded-2xl bg-gray-50 px-6 py-16 sm:p-16">
                 <div className="mx-auto max-w-xl lg:max-w-none">
                   <div className="text-center">
@@ -166,6 +245,85 @@ export default function Traduction() {
                           el proceso.
                         </p>
                       </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="relative isolate py-14">
+            <div className="mx-auto max-w-7xl px-8 lg:px-16">
+              <div className="mx-auto mt-8 grid auto-rows-fr grid-cols-1 gap-8 sm:mt-12 md:grid-cols-2">
+                <div className="flex">
+                  <div className="flex-auto justify-center items-center my-auto">
+                    <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl sm:text-center md:text-left">
+                      ¡Amplia gama de servicios de traducción para tus
+                      necesidades!
+                    </h2>
+                  </div>
+                  <div className="sm:hidden md:block md:flex-none justify-center items-center my-auto">
+                    <Image src={arrow} className="w-20 object-contain" alt="" />
+                  </div>
+                </div>
+                <div className="flex flex-col w-full">
+                  <div className="flex border rounded-full md:w-auto sm:w-full bg-sky-300 md:ml-24">
+                    <div className="flex-none my-auto justify-center items-center">
+                      <div className="border rounded-full bg-white ml-4 p-2 w-10 h-10">
+                        <Image src={traduction1} alt="" />
+                      </div>
+                    </div>
+                    <div className="flex-auto">
+                      <p className="text-center text-white p-4 font-bold">
+                        Traducciones Certificadas
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex border rounded-full md:w-auto sm:w-full footer-bg md:mr-24 mt-4">
+                    <div className="flex-auto">
+                      <p className="text-center text-white p-4 font-bold">
+                        Traducciones de documentos
+                      </p>
+                    </div>
+                    <div className="flex-none my-auto justify-center items-center">
+                      <div className="border rounded-full bg-white mr-4 p-2 w-10 h-10">
+                        <Image src={traduction2} alt="" />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex border rounded-full md:w-auto sm:w-full bg-sky-300 md:ml-24 mt-4">
+                    <div className="flex-none my-auto justify-center items-center">
+                      <div className="border rounded-full bg-white ml-4 p-2 w-10 h-10">
+                        <Image src={traduction3} alt="" />
+                      </div>
+                    </div>
+                    <div className="flex-auto">
+                      <p className="text-center text-white p-4 font-bold">
+                        Traducciones Certificadas
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex border rounded-full md:w-auto sm:w-full footer-bg md:mr-24 mt-4">
+                    <div className="flex-auto">
+                      <p className="text-center text-white p-4 font-bold">
+                        Traducciones de documentos
+                      </p>
+                    </div>
+                    <div className="flex-none my-auto justify-center items-center">
+                      <div className="border rounded-full bg-white mr-4 p-2 w-10 h-10">
+                        <Image src={traduction4} alt="" />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex border rounded-full md:w-auto sm:w-full bg-sky-300 md:ml-24 mt-4">
+                    <div className="flex-none my-auto justify-center items-center">
+                      <div className="border rounded-full bg-white ml-4 p-2 w-10 h-10">
+                        <Image src={traduction5} alt="" />
+                      </div>
+                    </div>
+                    <div className="flex-auto">
+                      <p className="text-center text-white p-4 font-bold">
+                        Traducciones Academicas
+                      </p>
                     </div>
                   </div>
                 </div>
